e0d33e1443c2d25cf4a1f56a936e18b4106fc7bc
Aleksandra-P94 <aleks9404@gmail.com>
2020-07-15 20:48:36 +0200

    next changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import style from "./scss/index.scss";
 import '../vendor/bootstrap-4.5.0/scss/bootstrap.scss';
 import icons from "../assets/img/airplane3.jpg"
+
 //dodatkowy panel - passengers
-var ShowSearchPassenger = document.getElementById('ShowSearchPassenger');
+let ShowSearchPassenger = document.getElementById('ShowSearchPassenger');
 
 ShowSearchPassenger.addEventListener('click', function () {
-    var x = document.getElementById("passengersAmount");
+    let x = document.getElementById("passengersAmount");
 
     if(x.style.display === "none"){
         x.style.display = "block";
@@ -14,42 +15,117 @@ ShowSearchPassenger.addEventListener('click', function () {
     }
 }); 
 
-var switchPlus = document.getElementById('plus');
-var switchMinus = document.getElementById('minus');
-var defaultNumber = document.getElementById('passengers');
-
+let switchPlus = document.getElementById('plus');
+let switchMinus = document.getElementById('minus');
+let defaultNumber = document.getElementById('passengers');
+let switchPlus2 = document.getElementById('plus2');
+let switchMinus2 = document.getElementById('minus2');
+let defaultNumber2 = document.getElementById('passengers2');
+let switchPlus3 = document.getElementById('plus3');
+let switchMinus3 = document.getElementById('minus3');
+let defaultNumber3 = document.getElementById('passengers3');
+//Adults
 switchPlus.addEventListener('click', function () {
     defaultNumber.value = addOne () +1;
-
+    
     if (defaultNumber.value > 9) {
         defaultNumber.value = 9;
     }
-}); 
-
-switchMinus.addEventListener(
-    'click', function () {
-                        defaultNumber.value = addOne () -1;
-                        if (defaultNumber.value <= 0) {
-                            defaultNumber.value = 0;
+});
+switchMinus.addEventListener('click', function () {
+    defaultNumber.value = addOne () -1;
+    
+    if (defaultNumber.value <= 0) {
+        defaultNumber.value = 0;
+    }})
+//Children
+switchPlus2.addEventListener('click', function () {
+    defaultNumber2.value = addOne2 () +1;
+    
+    if (defaultNumber2.value > 9) {
+        defaultNumber2.value = 9;
+    }
+    });
+switchMinus2.addEventListener('click', function () {
+    defaultNumber2.value = addOne2 () -1;
+    
+    if (defaultNumber2.value <= 0) {
+        defaultNumber2.value = 0;
+    }})
+//Infant
+switchPlus3.addEventListener('click', function () {
+    defaultNumber3.value = addOne3 () +1;
+            
+    if (defaultNumber3.value > 9) {
+        defaultNumber3.value = 9;
+    }
+        });
+switchMinus3.addEventListener('click', function () {
+    defaultNumber3.value = addOne3 () -1;
+            
+    if (defaultNumber3.value <= 0) {
+        defaultNumber3.value = 0;
     }})
 
 function addOne() {
     return parseInt(defaultNumber.value || 0, 10);
 };                                                          
+function addOne2() {
+    return parseInt(defaultNumber2.value || 0, 10);
+};     
+function addOne3() {
+    return parseInt(defaultNumber3.value || 0, 10);
+};   
+
+//confirm passengers
+let confirmPassengers=document.getElementById('okButton');
 
 
-//FROM_TO
-// function selectedValue(){
-//     var e = document.getElementById("selectC1").value;
-//     document.getElementById("from").innerHTML=e;
-// }
+confirmPassengers.addEventListener('click', function(){
+    document.getElementById('passengersAmount').style.display = "none";
+    let passengersAll = document.getElementById('passengersAll');
+    if (defaultNumber.value>0 || defaultNumber2.value>0 || defaultNumber3.value>0){
+        
+         passengersAll.innerHTML= defaultNumber.value + " adult " + defaultNumber2.value + " child " + defaultNumber3.value +" infant ";
+   
+     } });
+
+//zapisanie danych origin
+
+let origin = document.getElementById("selectCityOrigin");
+let selectOrigin = origin.options[origin.selectedIndex];
+let lastSelectedOrigin = localStorage.getItem('origin');
+
+if(lastSelectedOrigin){
+    origin.value = lastSelectedOrigin;
+}
+origin.onchange = function(){
+    lastSelectedOrigin = origin.options[origin.selectedIndex].value;
+    console.log(lastSelectedOrigin);
+    localStorage.setItem('origin', lastSelectedOrigin)
+};
+//zapisanie danych destination
+
+let destination = document.getElementById("selectCityDestination");
+let selectDestination = destination.options[origin.selectedIndex];
+let lastSelectedDestination = localStorage.getItem('destination');
+
+if(lastSelectedDestination){
+    destination.value = lastSelectedDestination;
+}
+destination.onchange = function(){
+    lastSelectedDestination = destination.options[destination.selectedIndex].value;
+    console.log(lastSelectedDestination);
+    localStorage.setItem('destination', lastSelectedDestination)
+};
+
 
 //FORM
 
 
 
 const inputName = document.getElementById("login").value;
-const inputPassword = document.getElementById("password").value;
+const inputPassword = document.getElementById("password");
 const inputEmail = document.getElementById("email");
 const emailRegExp = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
@@ -62,14 +138,14 @@ inputEmail.addEventListener("input", function (event) {
     }
 });
 //Password validation
-inputPassword.addEventListener("input", function(event)  {
-    if(inputPassword.length <= 4){
+inputPassword.addEventListener("input", function (event) {
+    if(inputPassword.validity.typeMismatch && length <= 4){
         inputPassword.setCustomValidity("Password must contain at least 6 characters, including UPPER/lowercase and numbers");
     } else {
         inputPassword.setCustomValidity("");
     }
 });
-    var data = '{"login": "Aleksandra", "password": "123qwe", "email": "aleksandra@gmail.com" }';
-    var mydata = JSON.parse(data);
+    let data = '{"login": "Aleksandra", "password": "123qwe", "email": "aleksandra@gmail.com" }';
+    let mydata = JSON.parse(data);
 
 
--- src/index2.js
+++ src/index2.js
@@ -2,8 +2,85 @@ import style from "./scss/index2.scss";
 import '../vendor/bootstrap-4.5.0/scss/bootstrap.scss';
 import icons from "../assets/img/airplane3.jpg";
 
-// fetch("airports_flights.json")
-//    .then((resp) => resp.json()) // Transform the data into json
-//    .then(function (data) {
-//      console.log(data);
-//    });
+
+
+
+
+document.getElementById("from").innerHTML=localStorage.getItem("origin");
+document.getElementById("to").innerHTML=localStorage.getItem("destination");
+
+//flights
+let theDeparture =[]; 
+let theArrival =[]; 
+let theDuration =[]; 
+let theHour=[]; 
+let thePrice=[];
+
+function checkFlight(){
+    let originV = localStorage.getItem("origin");
+    let destinationV = localStorage.getItem("destination");
+    console.log(originV, destinationV);
+    if (originV == "Berlin"){
+        
+    }
+
+};
+checkFlight();
+
+fetch("/airports_flights.json")
+    .then(function(resp){
+        return resp.json();
+    })
+    .then(function (data) {
+            
+        console.log(data.Berlin);
+
+        data.Berlin.forEach(function (thePrice) {
+            console.log(thePrice.price);   
+
+            
+                document.getElementById("price").innerHTML = `<tr> ${thePrice.price} </tr>`;
+            }) ;
+        data.Berlin.forEach(function (theArrival) {
+            console.log(theArrival.arrival)
+            for (var i=0; i < data.length; i++){
+                document.getElementsByClassName("arrival").innerHTML += `<tr> ${theArrival.arrival} </tr>`;
+            }});
+        data.Berlin.forEach(function (theDuration) {    
+             console.log(theDuration.duration)
+             for (var i=0; i < data.length; i++){
+                document.getElementsByClassName("duration").innerHTML += `<tr> ${theDuration.duration} </tr>`;
+            }});
+        data.Berlin.forEach(function (theHour) {     
+             console.log(theHour.hour)
+             for (var i=0; i < data.length; i++){
+                document.getElementsByClassName("hour").innerHTML += `<tr> ${theHour.hour} </tr>`;
+            }});
+        data.Berlin.forEach(function (theDeparture) {
+             console.log(theDeparture.departure) 
+             for (var i=0; i < data.length; i++){
+                document.getElementsByClassName("departure").innerHTML += `<tr> ${theDeparture.departure} </tr>`;
+            }});
+
+             //document.getElementById("price").innerHTML += `<tr> ${thePrice.price} </tr>`;
+     
+    
+
+
+
+
+//fetch("/airports_flights.json")
+    //.then(function(resp){
+   //     return resp.json();
+   // })
+  //  .then(function (data) {
+            
+   //     console.log(data);
+    //     data.forEach(function (thePrice) {
+    //         console.log(thePrice.price);
+    //         document.getElementById("price").innerHTML += `<tr> ${thePrice.price} </tr>`;
+    // }
+    //)
+
+//})
+        })
